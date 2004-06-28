4e79c064698bd13cc6f29d100f796164bed89179
jgdumas <jgdumas@dda9dfc7-2772-4c6c-972d-d99ad1e2f97f>
2004-06-28 13:36:26 +0000

    BUG templated Miller

--- src/kernel/integer/givintprime.C
+++ src/kernel/integer/givintprime.C
@@ -40,60 +40,8 @@ int FermatDom::pepin (const long n) const
 
 
 // =================================================================== //
-// Primality tests and factorization algorithms
-// =================================================================== //
-
-// =================================================================== //
 // Primality tests 
 // =================================================================== //
-
-template<class RandIter>
-int IntPrimeDom::Miller(RandIter& g, const Rep& n) const
-{
-        // Monte Carlo algorithm
-        // returns 1    : n prime with probability 3/4
-        // returns 0    : n composite
-    if (n < 2) return 0;
-    if (n <= 3) return 1;
-    IntPrimeDom::Rep t=n-1,a,q;
-    random(g,a,n);
-    long s=0;
-    for( ; !(t & 0x1) ; t<<=1, ++s) {}
-    powmod(q,a,t,n);
-    if ( (q==1) || (q == (n-1))) return 1;
-    for(;s>1;--s) {
-        q = (q*q) % n;
-        if (q == (n-1)) return 1;
-    }
-    return 0;
-}
-
-    
-template<class RandIter>
-IntPrimeDom::Rep& IntPrimeDom::test_Lehmann(RandIter& g, Rep& r, const Rep& n) const {
-        // Monte Carlo algorithm
-        // returns n-1  : n prime with probability 1/2
-        // returns 1    : n composite with probability 1/2
-        // else         : n composite
-    IntPrimeDom::Rep A;
-    random(g,A,n);
-    return powmod(r,A,(n-1)/2,n);
-}
-
-template<class RandIter>
-int IntPrimeDom::Lehmann(RandIter& g, const Rep& n)  const 
-{
-    if (n < 2) return 0;
-    if (n <= 3) return 1;
-    IntPrimeDom::Rep tmp;
-    IntPrimeDom::test_Lehmann(g,tmp,n);
-    if (tmp == (n-1))
-        return 1;
-    return 0;
-}
-
-
-
 IntPrimeDom::Rep& IntPrimeDom::nextprimein(Rep& n, int r)  const {
     if (isleq( n,1)) return n=2;
     Rep tmp;

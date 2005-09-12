b42415c0d26559cca1c901da976f0eea84355768
jgdumas <jgdumas@dda9dfc7-2772-4c6c-972d-d99ad1e2f97f>
2005-09-12 16:04:24 +0000

    *** empty log message ***

--- src/kernel/zpz/givtablelimits.h
+++ src/kernel/zpz/givtablelimits.h
@@ -3,9 +3,9 @@
 
 // 2^23 ---> 2^23*4*3 = 100K
 // #define FF_TABLE_MAX 8388608UL
-
-
 // 2^20 ---> 2s on 735MHz
-#define FF_TABLE_MAX 1048576UL
+//#define FF_TABLE_MAX 1048576UL
+// Now 2^21+1 seems OK
+#define FF_TABLE_MAX 2097153UL
 
 #endif

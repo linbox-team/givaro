220bff226de5f576dd2270208a5963894d48eba0
jgdumas <jgdumas@dda9dfc7-2772-4c6c-972d-d99ad1e2f97f>
2006-06-22 16:49:44 +0000

    *** empty log message ***

--- trunk/src/kernel/zpz/givcra.h
+++ trunk/src/kernel/zpz/givcra.h
@@ -5,7 +5,7 @@
 // Copyright(c)'94-97 by Givaro Team
 // see the copyright file.
 // Authors: T. Gautier
-// $Id: givcra.h,v 1.8 2006-06-22 16:47:57 jgdumas Exp $
+// $Id: givcra.h,v 1.9 2006-06-22 16:49:44 jgdumas Exp $
 // ==========================================================================
 // Description:
 //  Chinese Remainder Algorithm for 2 Elements. 
@@ -25,9 +25,6 @@ struct ChineseRemainder {
 	// Then C_12 = (M^-1 mod D)M
     ChineseRemainder(const Ring& R, const RingElement& M, const Domain& D) 
             : _domain(D)  {
-//         RingElement p;
-//         D.characteristic(p);
-//         P_12=  M * p ;
         DomainElement u;
         _domain.invin( _domain.init(u, M) );
         _domain.convert(C_12, u);
@@ -40,12 +37,8 @@ struct ChineseRemainder {
         // Then res mod M == A
         // And  res mod D == e
     RingElement & operator()( RingElement& res, const RingElement& A, const DomainElement& e) const {
-//         _domain.convert(res, e);
-//         res -= A;
-//         res *= C_12;
-//         res %= P_12;            // This is the reduction
         DomainElement smallA, smallM;
-        _domain.init(smallA, A);        // This is NOW the reduction
+        _domain.init(smallA, A);
         _domain.init(smallM);
         _domain.sub(smallM, e, smallA);
         _domain.convert(res, smallM);
@@ -56,7 +49,6 @@ struct ChineseRemainder {
 private:
     Domain _domain;
     RingElement C_12;
-//    RingElement P_12;
 };
 
 template<>

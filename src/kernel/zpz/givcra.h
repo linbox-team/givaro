41ce59f9f76cfc33fe77f7fb5522bb60c17c0cb2
jgdumas <jgdumas@dda9dfc7-2772-4c6c-972d-d99ad1e2f97f>
2006-06-22 16:47:57 +0000

    *** empty log message ***

--- src/kernel/zpz/givcra.h
+++ src/kernel/zpz/givcra.h
@@ -5,7 +5,7 @@
 // Copyright(c)'94-97 by Givaro Team
 // see the copyright file.
 // Authors: T. Gautier
-// $Id: givcra.h,v 1.7 2005-06-14 14:53:14 pernet Exp $
+// $Id: givcra.h,v 1.8 2006-06-22 16:47:57 jgdumas Exp $
 // ==========================================================================
 // Description:
 //  Chinese Remainder Algorithm for 2 Elements. 
@@ -21,36 +21,42 @@ struct ChineseRemainder {
     typedef typename Domain::Element DomainElement;
 
 
+        // Computes u = M^-1 in the domain
+	// Then C_12 = (M^-1 mod D)M
     ChineseRemainder(const Ring& R, const RingElement& M, const Domain& D) 
-            : _D(D)  {
-      RingElement p;
-      D.characteristic(p);
-      P_12=  M * p ;
+            : _domain(D)  {
+//         RingElement p;
+//         D.characteristic(p);
+//         P_12=  M * p ;
         DomainElement u;
-        _D.invin( _D.init(u, M) );
-        _D.convert(C_12, u);
+        _domain.invin( _domain.init(u, M) );
+        _domain.convert(C_12, u);
         C_12 *= M;
     }
 
 
 
-    RingElement & operator()( RingElement& res, const RingElement& A, const DomainElement& e) {
-//         _D.convert(res, e);
+        // Computes res = A + ((e-A) mod D)*(M^-1 mod D)M 
+        // Then res mod M == A
+        // And  res mod D == e
+    RingElement & operator()( RingElement& res, const RingElement& A, const DomainElement& e) const {
+//         _domain.convert(res, e);
 //         res -= A;
 //         res *= C_12;
 //         res %= P_12;            // This is the reduction
         DomainElement smallA, smallM;
-        _D.init(smallA, A);        // This is NOW the reduction
-        _D.init(smallM);
-        _D.sub(smallM, e, smallA);
-        _D.convert(res, smallM);
+        _domain.init(smallA, A);        // This is NOW the reduction
+        _domain.init(smallM);
+        _domain.sub(smallM, e, smallA);
+        _domain.convert(res, smallM);
         res *= C_12;
         return res += A;
     }
 
 private:
-    Domain _D;
-    RingElement C_12, P_12;
+    Domain _domain;
+    RingElement C_12;
+//    RingElement P_12;
 };
 
 template<>
@@ -60,26 +66,29 @@ struct ChineseRemainder<Ring, Domain, false>  {
     typedef typename Domain::Element DomainElement;
 
 
+        // Computes u = M^-1 in the domain
+	// Then C_12 = (M^-1 mod D)M
     ChineseRemainder(const Ring& R, const RingElement& M, const Domain& D) 
-            : _D(D) {
-
+            : _domain(D) {
         DomainElement u;
-        _D.invin( _D.init(u, M) );
-        _D.convert(C_12, u);
+        _domain.invin( _domain.init(u, M) );
+        _domain.convert(C_12, u);
         C_12 *= M;
     }
 
 
-
-    RingElement & operator()( RingElement& res, const RingElement& A, const DomainElement& e) {
-        _D.convert(res, e);
+        // Computes res = A + (e-A)*(M^-1 mod D)M
+        // Then res mod M == A
+        // And  res mod D == e
+    RingElement & operator()( RingElement& res, const RingElement& A, const DomainElement& e) const {
+        _domain.convert(res, e);
         res -= A;
         res *= C_12;
         return res += A;
     }
 
 private:
-    Domain _D;
+    Domain _domain;
     RingElement C_12;
 };
 

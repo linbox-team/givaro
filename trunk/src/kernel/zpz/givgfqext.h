2f561c726dbba35f18f42d62a069cc5b3adcdeb5
jgdumas <jgdumas@dda9dfc7-2772-4c6c-972d-d99ad1e2f97f>
2007-11-16 16:48:16 +0000

    Binary Tables

--- trunk/src/kernel/zpz/givgfqext.h
+++ trunk/src/kernel/zpz/givgfqext.h
@@ -3,7 +3,7 @@
 
 // ==========================================================================
 // file: givgfqext.h 
-// Time-stamp: <16 Nov 07 17:07:27 Jean-Guillaume.Dumas@imag.fr>
+// Time-stamp: <16 Nov 07 17:45:36 Jean-Guillaume.Dumas@imag.fr>
 // (c) Givaro Team
 // date: 2007
 // version: 
@@ -43,17 +43,17 @@ public:
 
         // Extension MUST be a parameter of the constructor
     GFqExt( const UTT P, const UTT e) : Father_t(P,e),
-                                        
+                                        balanced(false),
         _BITS( std::numeric_limits< double >::digits/( (e<<1)-1) ), 
         _BASE(1 << _BITS),
         _MASK( _BASE - 1),
         _maxn( _BASE/(P-1)/(P-1)/e),
-                                        _degree( e-1 ),
-                                        balanced(false)
+                                        _degree( e-1 )
     {
 
         GIVARO_ASSERT(_maxn>0 , "[GFqExt]: field too large");
         builddoubletables();
+        
     }
 
     Self_t operator=( const Self_t& F)
@@ -97,20 +97,33 @@ public:
     UTT& characteristic(UTT& a) const { return a=this->_characteristic; }
     const bool balanced;
             
+    Rep& init( Rep& r, const unsigned long l) const {
+        return Father_t::init(r,l);
+    }
+    
+           
+    using Father_t::init;
+    
             
-    virtual double& convert(double& d, const Rep& a) const {
-//         this->write(std::cerr, a) << " -->-- " << _log2dbl[a] << std::endl;
+    virtual double& convert(double& d, const Rep a) const {
         return d=_log2dbl[a];
     }        
 
-    virtual float& convert(float& d, const Rep& a) const {
+    virtual float& convert(float& d, const Rep a) const {
         return d=(float)_log2dbl[a];
     }
 
-    virtual Rep& init(Rep& pad, const double& d) const {
-        unsigned __GIVARO_INT64 rll(d);
-        unsigned __GIVARO_INT64 tll(this->_inversecharacteristic*d);
-        UTT mpp(1), prec(0); 
+    virtual Rep& init(Rep& pad, const double d) const {
+//             // Precondition : d < _MODOUT
+//         unsigned __GIVARO_INT64 rll(d); 
+//         unsigned __GIVARO_INT64 tll(this->_inversecharacteristic*d);
+            // JGD 16.11.2007 : Much slower but defensive
+            // What shall we do ?
+        unsigned __GIVARO_INT64 rll(d); 
+        rll &= _MODOUT;
+        unsigned __GIVARO_INT64 tll(this->_inversecharacteristic*rll);
+
+        UTT prec(0); 
         UTT padl = (UTT)(rll - tll*this->_characteristic);
         if (padl == this->_characteristic) {
             padl -= this->_characteristic;
@@ -119,18 +132,20 @@ public:
         for(size_t j = 0;j<_degree;++j) {
             rll >>= _BITS;
             tll >>= _BITS;
-            mpp *= this->_characteristic;
             prec = (UTT)(rll-tll*this->_characteristic);
-            padl += prec * mpp;
+
+            padl <<= _pceil;
+            padl += prec;
         }
-        mpp = 1;
+
         pad = prec;
         for(size_t j = 0;j<_degree;++j) {
             rll >>= _BITS;
             tll >>= _BITS;
-            mpp *= this->_characteristic;
             prec = (UTT)(rll-tll*this->_characteristic);
-            pad += prec * mpp;
+
+            pad <<= _pceil;
+            pad += prec;
         }
         
         padl = this->_low2log[padl];
@@ -138,9 +153,9 @@ public:
         return this->addin(pad,padl);
     }
     
-     virtual Rep& init(Rep& pad, const float& d) const {
-         return init(pad, (double)d);
-     }
+    virtual Rep& init(Rep& pad, const float d) const {
+        return init(pad, (double)d);
+    }
     
            
 
@@ -152,6 +167,8 @@ protected:
     UTT _MASK;
     UTT _maxn;
     UTT _degree;
+    UTT _pceil;
+    UTT _MODOUT;
    
     
         // Conversion tables from exponent to double z-adic representation
@@ -162,16 +179,20 @@ protected:
 
     void builddoubletables() {
         _log2dbl.resize(this->_log2pol.size());
-        _high2log.resize(this->_log2pol.size());
-        _low2log.resize(this->_log2pol.size());
+        _pceil = 1;
+        for(ulong ppow = 2; ppow < this->_characteristic; ppow <<= 1,++_pceil) {}
+        ulong powersize = 1<<(_pceil * this->_exponent);
+        _MODOUT = powersize - 1;
+        _high2log.resize(powersize);
+        _low2log.resize(powersize);
+
+       
 
         typedef typename Father_t::Element ZElem;
         Father_t Zp(this->_characteristic,1);
         ZElem q,mq; Zp.init(q,2UL);
         dom_power(q,q,_BITS,Zp);
-//         Zp.write(std::cerr << "q: ",q) << std::endl;
         Zp.neg(mq,q);
-//         Zp.write(std::cerr << "-q: ",mq) << std::endl;
 
         Element xkmu;
             // This is X
@@ -189,8 +210,7 @@ protected:
          
         Father_t Z2B(2,_BITS); 
         PolDom P2dom( Z2B );
-        PadicDom P2AD( P2dom );
-        
+        PadicDom P2AD( P2dom );        
 
         std::vector<double>::iterator dblit = _log2dbl.begin();
         typename std::vector<UTT>::const_iterator polit = this->_log2pol.begin();
@@ -208,9 +228,11 @@ protected:
                                  this->_exponent)
                              );
 
-//             std::cerr << *polit << "  --->  ";
-//             for(size_t i = 0; i<this->_exponent; ++i)
-//                 std::cerr << vect[i] << ' ';
+            ulong binpolit = vect[0];
+            for(size_t i =1; i<this->_exponent; ++i) {
+                binpolit <<= _pceil;
+                binpolit += vect[i];
+            }
 
             ZElem tmp, prec, cour; 
             Zp.init(prec, vect[0]);
@@ -218,39 +240,16 @@ protected:
                 Zp.init(cour, vect[i]);
                 Zp.axpy(tmp, mq, cour, prec);    
                 low_ui.push_back(tmp);
-//                 low_ui.push_back( 
-//                     Zp.axpy(tmp, mq, prec, 
-//                             Zp.init(cour, vect[i])
-//                     );
                 prec = cour;
             }
-
-//             std::cerr << "  --->  ";
-//             for(size_t i = 0; i<low_ui.size(); ++i)
-//                 Zp.write(std::cerr,low_ui[i]) << ' ';
             
             PAD.eval(tmp , low_ui );
-//             std::cerr << "  --->  " << tmp ;
+            _low2log[binpolit] = this->_pol2log[tmp];
 
-            _low2log[*polit] = this->_pol2log[tmp];
-
-
-
-            low_ui.push_back(cour); 
-
-//             std::cerr << "  --->  ";
-//             for(size_t i = 0; i<low_ui.size(); ++i)
-//                 Zp.write(std::cerr,low_ui[i]) << ' ';
-
+            low_ui.push_back(cour);
             PAD.eval( tmp, low_ui);
-//             std::cerr << "  --->  " << tmp ;
-            
-//             std::cerr << std::endl;
-            Father_t::mul((Element&)_high2log[*polit],(Element)this->_pol2log[tmp], xkmu);
+            Father_t::mul((Element&)_high2log[binpolit],(Element)this->_pol2log[tmp], xkmu);
         }
-
-
-        
     }
 };
 

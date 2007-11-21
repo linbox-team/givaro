e9068ee2b493df729b5fb791fc0f0d1a2bb99463
jgdumas <jgdumas@dda9dfc7-2772-4c6c-972d-d99ad1e2f97f>
2007-11-21 10:29:42 +0000

    Example of irreducible access

--- trunk/examples/FiniteField/GF128.C
+++ trunk/examples/FiniteField/GF128.C
@@ -1,5 +1,5 @@
 // ========================================================== //
-// Time-stamp: <16 Nov 07 10:26:10 Jean-Guillaume.Dumas@imag.fr>
+// Time-stamp: <21 Nov 07 10:46:32 Jean-Guillaume.Dumas@imag.fr>
 // Thanks to Dieter Schuster
 // ========================================================== //
 #include <givaro/givgfq.h>
@@ -22,35 +22,47 @@ int main(int argc, char** argv)
     F2.init(P[0],1);
     F2.init(P[1],1);
     GF128.init(f, P);
-    GF128.write(std::cout << "2-adic representation of 1+X is: ", f);
-    std::cout << " ... while its internal representation is: " << f << std::endl;
+    GF128.write(std::cout << "2-adic representation of 1+X is: ", f)
+                          << std::endl
+                          << " ... while its internal representation is: " 
+                          << f << std::endl;
 
-
-    gen = 1;
     GF128.write(std::cout << "Indeed, we are in ") <<std::endl;
-    GF128.write(std::cout << "In this field, the generator used is (in 2-adic): ", gen)
-        << " whose internal representation is " << gen << std::endl;
+
+    GF128.generator(gen);
+    GF128.write(std::cout << 
+                "In this field, the generator used is (in 2-adic): ", gen)
+                          << std::endl
+                          << " whose internal representation is " 
+                          << gen << std::endl;
 
 
     Poly1PadicDom< GFqDom<long>, Dense > Padic2(Pol2);
         // 
 
-    std::cout << "Irreducible: " << GF128.irreducible() << std::endl;
-    std::cout << "Now, the irreducible polynomial used to build this field is (in 2-adic)" << std::endl;
-
+    std::cout << "Irreducible (in 2-adic): " 
+              << GF128.irreducible() << std::endl;
 
     GF128.init(g, Padic2.radix( Q, Integer(5) ));
-    GF128.write(std::cout << "2-adic representation of 1+X^2 is: ", g) << std::endl;
+    GF128.write(std::cout 
+                << "2-adic representation of 1+X^2 is: ", g) 
+                << std::endl;
 
     GF128.init(h);
     GF128.add(h, g, f);
-    GF128.write(std::cout << "2-adic representation of X+X^2 is: ", h) << std::endl;   
+    GF128.write(std::cout 
+                << "2-adic representation of X+X^2 is: ", h) 
+                << std::endl;   
 
     GF128.mul(h, g, f);
-    GF128.write(std::cout << "2-adic representation of 1+X+X^2+X^3 is: ", h) << std::endl;    
+    GF128.write(std::cout 
+                << "2-adic representation of 1+X+X^2+X^3 is: ", h) 
+                << std::endl;    
 
     GF128.div(h, g, f);
-    GF128.write(std::cout << "2-adic representation of 1+X is: ", h) << std::endl;
+    GF128.write(std::cout 
+                << "2-adic representation of 1+X is: ", h) 
+                << std::endl;
     
     return 0;
     

fd3de3d100afdd9791450b9e087fbda8e1d9bf7a
jgdumas <jgdumas@dda9dfc7-2772-4c6c-972d-d99ad1e2f97f>
2008-06-23 13:44:02 +0000

    patch for icc with static members

--- src/library/poly1/givdegree.C
+++ src/library/poly1/givdegree.C
@@ -3,11 +3,13 @@
 // Copyright(c)'94-97 by Givaro Team
 // see the copyright file.
 // Authors: T. Gautier
-// $Id: givdegree.C,v 1.1.1.1 2004-05-12 16:08:24 jgdumas Exp $
+// $Id: givdegree.C,v 1.2 2008-06-23 13:44:02 jgdumas Exp $
 // ==========================================================================
 
 #include "givaro/givdegree.h"
 
 // -- Degree of zero polynomial
+#ifndef __ECC
 const long Degree::deginfty = DEGPOLYZERO;
+#endif
 

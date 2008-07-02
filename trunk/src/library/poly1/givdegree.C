073efdfbc6b80008b60612526ce78ca61c89ee80
jgdumas <jgdumas@dda9dfc7-2772-4c6c-972d-d99ad1e2f97f>
2008-07-02 12:54:49 +0000

    givdegree.C not required anymore

--- trunk/src/library/poly1/givdegree.C
+++
@@ -1,9 +0,0 @@
-// ==========================================================================
-// $Source: /var/lib/cvs/Givaro/src/library/poly1/givdegree.C,v $
-// Copyright(c)'94-97 by Givaro Team
-// see the copyright file.
-// Authors: T. Gautier
-// $Id: givdegree.C,v 1.3 2008-07-02 12:53:02 jgdumas Exp $
-// ==========================================================================
-
-#include "givaro/givdegree.h"

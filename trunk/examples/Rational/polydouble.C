b0a6e1d16b97fd5df89ea4607ccccb6975bdd223
jgdumas <jgdumas@dda9dfc7-2772-4c6c-972d-d99ad1e2f97f>
2008-07-18 12:40:21 +0000

    added polydouble.C

---
+++ trunk/examples/Rational/polydouble.C
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <stdlib.h>
+#include <givaro/givpoly1.h>
+#include <givaro/givrational.h>
+#include <givaro/givtimer.h>
+#include <givaro/givinit.h>         // Givaro initialization
+
+typedef Poly1Dom< RationalDom, Dense>::Element RatPoly;
+typedef std::vector<double> DoublePoly;
+    
+std::ostream& operator<< (std::ostream& o, const RatPoly& v) {
+    o << "Poly (s [v_1, ..,  v_s]) : ";
+    o << v.size() << " [";
+    for(size_t i=0; i<v.size(); ++i) {
+        o << ' ' << v[i];
+    }
+    return o << ']';
+}    
+
+std::ostream& operator<< (std::ostream& o, const DoublePoly& v) {
+    o << '[';
+    for(size_t i=0; i<v.size(); ++i) {
+        o << ' ' << v[i];
+    }
+    return o << ']';
+}    
+
+int main(int argc, char** argv)
+{
+    srand48(BaseTimer::seed() );
+    
+
+    Integer f,m,k;
+
+    RationalDom Q;
+    Poly1Dom< RationalDom, Dense> PolQ(Q);
+    
+
+    DoublePoly D;
+    RatPoly R;
+    
+
+    size_t n = (argc>1?atoi(argv[1]):10);
+    
+    for(size_t i=0;i<n;++i) 
+        D.push_back( drand48() );
+    
+    
+    R.resize( D.size() );
+    
+    RatPoly::iterator it=R.begin();
+    DoublePoly::const_iterator dit = D.begin();
+    for( ; dit != D.end(); ++dit, ++it) 
+        *it =*dit;
+        
+    std::cout << "Double Poly : " << D << std::endl;
+    std::cout << "REDUCED Rational " << R << std::endl;
+    std::cout << "Approximations : ";
+        
+    RatPoly::const_iterator cit=R.begin();
+    dit = D.begin();
+    for( ; dit != D.end(); ++dit, ++cit) 
+        std::cout << std::endl << *cit << " is " << ((double)*cit) << " by " << ( (double)*cit -*dit ) << ' ';
+    std::cout << std::endl;
+    
+        
+
+    Rational::SetNoReduce();
+
+    it=R.begin();
+    dit = D.begin();
+    for( ; dit != D.end(); ++dit, ++it) 
+        *it =*dit;
+
+    std::cout << "Double Poly : " << D << std::endl;
+    std::cout << "Unreduced Rational " << R << std::endl;
+    std::cout << "Approximations : ";
+        
+    cit=R.begin();
+    dit = D.begin();
+    for( ; dit != D.end(); ++dit, ++cit) 
+        std::cout << std::endl << *cit << " is " << ((double)*cit) << " by " << ( (double)*cit -*dit ) << ' ';
+    std::cout << std::endl;
+    
+        
+ 
+    return 0;
+}
+

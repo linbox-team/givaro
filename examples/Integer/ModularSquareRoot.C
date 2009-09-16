aee1152fc3e383426d021c882defb2f92df4c66f
jgdumas <jgdumas@dda9dfc7-2772-4c6c-972d-d99ad1e2f97f>
2009-09-16 16:31:29 +0000

    modular square root prototype

--- examples/Integer/ModularSquareRoot.C
+++ examples/Integer/ModularSquareRoot.C
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <stdlib.h>
-#include <givaro/givintprime.h>
-#include <givaro/givrandom.h>
+#include <givaro/givintsqrootmod.h>
 #include <givaro/givtimer.h>
 
 // Algorithm 3.34 (Square Root Mod p) of 
@@ -10,46 +9,29 @@
 
 int main(int argc, char** argv)
 {
-    Integer a(argv[1]), p(argv[2]);
-    std::cerr << "p: " << p << std::endl;
+    Integer a(argv[1]), n(argv[2]);
+    std::cerr << "n: " << n << std::endl;
     std::cerr << "a: " << a << std::endl;
-    if ( legendre( a , p ) == -1 ) 
-        std::cerr << "Not a quadratic residue" << std::endl;
-    else {
-        Timer chrono; chrono.start();
-
-        Integer t(p-1);
-        size_t s=0;
-        for( ; ((t>>1)<<1) == t ; ++s) t>>=1;
     
-        size_t l=(size_t)ceil(logtwo(p));
+    
+    if ( legendre( a , n ) == -1 ){ 
+        std::cerr << "Not a quadratic residue" << std::endl;
+   }
+   else {
+        IntSqrtModDom<> ISM;
 
-        Integer::seeding( BaseTimer::seed() );
-        Integer b; 
-        while ( legendre( Integer::random(b,l), p ) != -1 ) ;
-
-        Integer inva; inv(inva, a, p);
-
-        Integer c; powmod(c, b, t, p);
-        Integer r; powmod(r, a, (t+1)>>1, p);
-        
-        for(size_t i=1; i<s; ++i) {
-            Integer d(r);
-            d *= d; d %= p;
-            d *= inva; d %= p;
-            powmod(d, d, 1<<(s-i-1), p);
-            if (d == p-1) {
-                r *= c; r %= p;
-            }
-            c *= c ; c %= p;
-        }
-        chrono.stop();
-
-        std::cout << r << std::endl;
+	Integer r;
+	Timer chrono; chrono.start();
+	ISM.sqrootmod(r,a,n);
+	chrono.stop();	
+	std::cout << r << std::endl;
         std::cerr << chrono << std::endl;
         
-        std::cerr << "Check, " << r << "^2 mod " << p << " = " << ( (r*r)%p) << std::endl;
-    }
+        std::cerr << "Check, " << r << "^2 mod " << n << " = " << ( (r*r)%n) << std::endl;	
+
+        }
+        
+
     
     return 0;
 }

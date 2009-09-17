36ba0b9bfd2efd553b677591f03e713aea679ada
jgdumas <jgdumas@dda9dfc7-2772-4c6c-972d-d99ad1e2f97f>
2009-09-17 16:08:34 +0000

    corrected a bug in sqrttwo*

--- trunk/examples/Integer/ModularSquareRoot.C
+++ trunk/examples/Integer/ModularSquareRoot.C
@@ -1,8 +1,12 @@
+// =================================================================== //
 // Copyright(c)'1994-2009 by The Givaro group
 // This file is part of Givaro.
 // Givaro is governed by the CeCILL-B license under French law
 // and abiding by the rules of distribution of free software. 
 // see the COPYRIGHT file for more details.
+// Time-stamp: <17 Sep 09 15:52:53 Jean-Guillaume.Dumas@imag.fr> 
+// Givaro : Modular square roots
+// =================================================================== //
 
 #include <iostream>
 #include <stdlib.h>
@@ -18,26 +22,18 @@ int main(int argc, char** argv)
     Integer a(argv[1]), n(argv[2]);
     std::cerr << "n: " << n << std::endl;
     std::cerr << "a: " << a << std::endl;
+
+    Integer::seeding (BaseTimer::seed ());
+    IntSqrtModDom<> ISM;
     
+    Integer r;
+    Timer chrono; chrono.start();
+    ISM.sqrootmod(r,a,n);
+    chrono.stop();	
+    std::cout << r << std::endl;
+    std::cerr << chrono << std::endl;
     
-    if ( legendre( a , n ) == -1 ){ 
-        std::cerr << "Not a quadratic residue" << std::endl;
-   }
-   else {
-        IntSqrtModDom<> ISM;
-
-	Integer r;
-	Timer chrono; chrono.start();
-	ISM.sqrootmod(r,a,n);
-	chrono.stop();	
-	std::cout << r << std::endl;
-        std::cerr << chrono << std::endl;
-        
-        std::cerr << "Check, " << r << "^2 mod " << n << " = " << ( (r*r)%n) << std::endl;	
-
-        }
-        
-
+    std::cerr << "Check, " << r << "^2 mod " << n << " = " << ( (r*r)%n) << std::endl;	
     
     return 0;
 }

6f82cb2d7e8a7401632d1a76703effa0b5fbea00
jgdumas <jgdumas@dda9dfc7-2772-4c6c-972d-d99ad1e2f97f>
2011-01-20 10:04:22 +0000

    added Siguna Mueller trick for p = 9 mod 16 simplified and optimized Tonelli-Shanks added tests with large power

--- trunk/tests/test-modsqroot.C
+++ trunk/tests/test-modsqroot.C
@@ -4,7 +4,7 @@
 // Givaro is governed by the CeCILL-B license under French law
 // and abiding by the rules of distribution of free software.
 // see the COPYRIGHT file for more details.
-// Time-stamp: <18 Jan 11 14:37:04 Jean-Guillaume.Dumas@imag.fr>
+// Time-stamp: <20 Jan 11 11:04:19 Jean-Guillaume.Dumas@imag.fr>
 // Givaro : Modular square roots
 // =================================================================== //
 
@@ -19,36 +19,56 @@
 #include <givaro/givintsqrootmod.h>
 #include <givaro/givtimer.h>
 
+IntSqrtModDom<> ISM;
+
+bool TestSrqtMod(const Integer& z, const Integer& n) {
+    Integer a(z),b,x,r;
+    ISM.modin(a,n);
+    ISM.modin(ISM.mul(b,a,a),n); // b is now a square mod n
+    ISM.sqrootmod(x,b,n);
+    if (ISM.areEqual( ISM.modin(ISM.mul(r,x,x),n), b) ) {
+        return true;
+    } else {
+        std::cerr << "TestSrqtMod ERROR" << std::endl;
+        std::cerr << "n:= " << n << ';' << std::endl;
+        std::cerr << "a:= " << a << ';' << std::endl;
+        std::cerr << "x:= " << x << ';' << std::endl;
+        std::cerr << "r:= " << r << ';' << std::endl;
+        
+        return false;
+    }
+}
+
 int main(int argc, char** argv) {
-    
     int nbtests = (argc>1?atoi(argv[1]):100);
     int sizes = (argc>2?atoi(argv[2]):10);
+    unsigned long seed = (argc>3?atoi(argv[3]):BaseTimer::seed ());
     int failures = 0;
-    Integer::seeding (BaseTimer::seed ());
-    IntSqrtModDom<> ISM;
-    Integer r,x,a,b,n;
+//     std::cerr << "Seed: " << seed << std::endl;
+    Integer::seeding (seed);
+    Integer a,n;
+    Integer ThreeToHundred;
+    pow(ThreeToHundred,Integer(3),100UL);
 
     for(int i=0; i<nbtests; ++i) {
         
         Integer::random(a,sizes);
         Integer::nonzerorandom(n,sizes);
-        while (n<=1) Integer::nonzerorandom(n,sizes);
+        while(n<=1) Integer::nonzerorandom(n,sizes);
 
-        ISM.modin(a,n);
+        if (! TestSrqtMod(a,n)) ++failures;
         
-        ISM.modin(ISM.mul(b,a,a),n); // b is now a square mod n
-        ISM.sqrootmod(x,b,n);
-        if (! ISM.areEqual( ISM.modin(ISM.mul(r,x,x),n), b) ) {
-            
-            std::cerr << "n: " << n << std::endl;
-            std::cerr << "a: " << a << std::endl;
-            std::cerr << "x: " << x << std::endl;
-            std::cerr << "r: " << r << std::endl;
-            
-            ++failures;
-        }
+        n <<= (129);
+        n *= ThreeToHundred;
+
+//         ISM.write(std::cerr, n) << std::endl;        
+        
+        if (! TestSrqtMod(a,n)) ++failures;        
+        
         
     }
      
+    if (failures > 0) std::cerr << "test-modsqroot: " << failures << " failures." << std::endl;
+    
     return failures;
 }

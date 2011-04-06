ee64d0498e85ae7044bda3ad74199404addcb0ac
jgdumas <jgdumas@dda9dfc7-2772-4c6c-972d-d99ad1e2f97f>
2011-04-06 15:44:29 +0000

    added interpolation of a vector of polynomials

--- trunk/tests/test-geom.C
+++ trunk/tests/test-geom.C
@@ -4,7 +4,7 @@
 // and abiding by the rules of distribution of free software. 
 // see the COPYRIGHT file for more details.
 // -------------------------------------------------------------
-// Time-stamp: <18 Feb 11 16:11:21 Jean-Guillaume.Dumas@imag.fr>
+// Time-stamp: <06 Apr 11 17:42:23 Jean-Guillaume.Dumas@imag.fr>
 //                                                              
 // Interpolation at geometric points                            
 // see: Polynomial evaluation and interpolation on special sets 
@@ -19,6 +19,7 @@
 #include <givaro/givgfq.h>
 #include <givaro/givpoly1.h>
 #include <givaro/givinterpgeom.h>
+#include <givaro/givinterpgeom-multip.h>
 
 
 #ifdef GIVARO_DEBUG
@@ -43,6 +44,34 @@ struct BlackBoxPolynomial {
     }
     
 };
+
+
+struct BlackBoxVectorOfPolynomial {
+    typedef Poly1Dom<GFqDom<long>,Dense > PolDom_t;
+    typedef PolDom_t::Element	    Polynomial;
+    typedef PolDom_t::Type_t	    Type_t;
+    typedef std::vector< Type_t > Vect_t;
+    typedef std::vector< Polynomial > VectPoly_t;
+    
+    const PolDom_t& _PD;
+    VectPoly_t _func;
+    
+
+    BlackBoxVectorOfPolynomial(const PolDom_t& pold, VectPoly_t& P) 
+            : _PD(pold), _func(P) 
+        {}
+    
+    Vect_t& operator()(Vect_t& vi, const Type_t& xi) const {
+        vi.resize(_func.size());
+        Vect_t::iterator iter_vi = vi.begin();
+        VectPoly_t::const_iterator iter_func = _func.begin();
+        for( ; iter_func != _func.end(); ++iter_func, ++iter_vi)
+            _PD.eval(*iter_vi, *iter_func, xi);
+        return vi;
+    }
+    
+};
+
 
 template<class Interp, class RandIter>
 bool TestOneField(Interp& FD, RandIter& generator, size_t degmax) {
@@ -80,15 +109,59 @@ bool TestOneField(Interp& FD, RandIter& generator, size_t degmax) {
 }
 
 
+template<class Interp, class RandIter>
+bool TestOneFieldVect(Interp& FD, RandIter& generator, size_t degmax, size_t numpol) {
+
+    std::vector<typename Interp::Polynomial_t> nouv(numpol), prec(numpol);
+    for(size_t i=0; i< numpol; ++i) {
+        FD.getpoldomain().random(generator, prec[i], Degree(degmax) );
+#ifdef GIVARO_DEBUG
+        FD.getpoldomain().write(std::cout << "Precedent[" << i << "]: ", prec[i]) << std::endl;
+#endif
+    }
+    
+    BlackBoxVectorOfPolynomial TestBB(FD.getpoldomain(), prec);
+    
+    FD.initialize(TestBB);
+    for(size_t i=1; i<=degmax; ++i)
+        FD(TestBB);
+        
+
+    FD.interpolator(nouv);
+    
+#ifdef GIVARO_DEBUG
+    for(size_t i=0; i< numpol; ++i)
+        FD.getpoldomain().write(std::cout << "Nouveau[" << i << "]: ", nouv[i]) << std::endl;
+#endif 
+    
+    
+    
+    for(size_t i=0; i< numpol; ++i)
+        if (! FD.getpoldomain().areEqual(prec[i],nouv[i])) {
+#ifdef GIVARO_DEBUG
+            std::cout << "ERREUR: Precedent[" << i << "] != Nouveau[" << i << ']'  << std::endl;
+#endif
+            return false;
+        }
+
+#ifdef GIVARO_DEBUG
+    ++TTcount;
+#endif
+    return true;
+}
+
+
 int main(int argc, char ** argv) {
         // argv[1] : modulo
         // argv[2] : deg max
-        // argv[3] : seed
+        // argv[3] : num poly max
+        // argv[4] : seed
 
     GFqDom<long>::Residu_t MOD = (argc>1 ? atoi(argv[1]) : 101);
     size_t degmax = (argc>2 ? atoi(argv[2]) : 20);
+    size_t numpolmax = (argc>3 ? atoi(argv[3]) : 15);
 
-    size_t seed = (argc>3?atoi(argv[3]):BaseTimer::seed());
+    size_t seed = (argc>4?atoi(argv[4]):BaseTimer::seed());
 #ifdef GIVARO_DEBUG
     std::cerr << "seed: " << seed << std::endl;
 #endif
@@ -96,12 +169,19 @@ int main(int argc, char ** argv) {
 
     GFqDom<long> F(MOD);
     NewtonInterpGeom< GFqDom<long> > FD(F, Indeter("X"));
+    NewtonInterpGeomMultip< GFqDom<long> > FDM(F, Indeter("X"));
 
     bool success = true;
+
     for(size_t d=1; d<degmax ; ++d)
         for(size_t i=0; i<50; ++i)
             success &= TestOneField(FD, generator, d);
 
+    for(size_t p=1; p<numpolmax ; ++p)
+        for(size_t d=1; d<degmax ; ++d)
+            for(size_t i=0; i<5; ++i)
+                success &= TestOneFieldVect(FDM, generator, d, p);
+    
 #ifdef GIVARO_DEBUG
     if (! success) {
         std::cerr << "Error: " << seed << std::endl;

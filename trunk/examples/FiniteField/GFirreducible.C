4b46109cf36d785882caffc816d023bb4cdd5697
jgdumas <jgdumas@dda9dfc7-2772-4c6c-972d-d99ad1e2f97f>
2011-05-03 15:30:36 +0000

    added extension of non GfqDom field

--- trunk/examples/FiniteField/GFirreducible.C
+++ trunk/examples/FiniteField/GFirreducible.C
@@ -4,7 +4,7 @@
 // Givaro is governed by the CeCILL-B license under French law
 // and abiding by the rules of distribution of free software.
 // see the COPYRIGHT file for more details.
-// Time-stamp: <09 Apr 08 15:35:30 Jean-Guillaume.Dumas@imag.fr>
+// Time-stamp: <03 May 11 17:30:35 Jean-Guillaume.Dumas@imag.fr>
 // Check of irreducible polynomial and generators of GFq
 // ========================================================== //
 //
@@ -49,31 +49,32 @@ int main(int argc, char** argv)
     GFq.init(tmp);
 
     GFq.mul(tmp, temo, t); // internal representation of X^e
-    GFq.negin(tmp);
+    GFq.negin(tmp); 	   // internal representation of -X^e, i.e. of the irreducible polynomial without the largest monomial, X^e
 
-    long irreducible;
-    GFq.convert(irreducible,tmp);
-    std::cout << irreducible << std::endl;
+    long lowerpart;
+        // p-adic value of the lower part of the irreducible polynomial  
+    GFq.convert(lowerpart, tmp);
+    std::cout << ' ' << p << "-adic value of the lower part of the irreducible : " << lowerpart << std::endl;
 
-    long ptoe = power(p,e);
+    long ptoe = power(p,e); // p-adic value of X^e
+    std::cout << ' ' << p << '^' << e << " is : " << ptoe << std::endl;
 
-    std::cout << "Computed irreducible: " << ptoe+irreducible << std::endl;
-    std::cout << "Stored   irreducible: " << GFq.irreducible() << std::endl;
-
-
+    std::cout << " --> Computed irreducible: " << ptoe+lowerpart << std::endl;
+    std::cout << "Stored        irreducible: " << GFq.irreducible() << std::endl;
 
     Poly1PadicDom< GFqDom<long>, Dense > PAD(Pdom);
-    Poly1PadicDom< GFqDom<long>, Dense >::Element Polynomial, Generator;
+    Poly1PadicDom< GFqDom<long>, Dense >::Element Polynomial;
 
     PAD.radix(Polynomial, GFq.irreducible());
 
+    std::cout << "Irreducible polynomial coefficients: ";
     for(Poly1PadicDom< GFqDom<long>, Dense >::Element::iterator it = Polynomial.begin(); it != Polynomial.end(); ++it)
-        std::cout << ' ' << *it;
+        PrimeField.write(std::cout << ' ', *it);
     std::cout << std::endl;
 
 
-    PAD.write(std::cout << "The latter represents: ", Polynomial)
-                        << " in p-adic"
+    PAD.write(std::cout << "The latter " << GFq.irreducible() << " represents: ", Polynomial)
+                        << " in " << p << "-adic"
                         << std::endl;
 
 

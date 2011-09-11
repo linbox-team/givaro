2ee5039352bfad8d3e8f58532d377ad07a46690b
bboyer <bboyer@dda9dfc7-2772-4c6c-972d-d99ad1e2f97f>
2011-09-11 13:25:08 +0000

    Integer cast problem. (uint)(Integer) -1 == 1. (llint)(Integer) -1 = 1 and many others....

--- trunk/tests/test-conversion.C
+++ trunk/tests/test-conversion.C
@@ -7,20 +7,57 @@
 
 
 #include <givaro/givinteger.h>
+#include <math.h>
+
 using namespace Givaro;
+
+int testBasicConversion()
+{
+	int err = 0;
+	/* test small neg int */
+	int qq = -1 ;
+	Integer q(qq);
+	int q1  = (int) q;
+	if (q1 != qq)
+		err  = 1;
+
+	long int q2 = (long int) q;
+	if (q2 != qq)
+		err  = 2;
+
+#ifndef __GIVARO__DONOTUSE_longlong__
+	long long int q3 = (long long int) q;
+	if (q3 != qq)
+		err = 3 ;
+#endif
+
+	/*  test unsigned versions */
+	unsigned int uqq = (unsigned int) qq;
+	unsigned int q4  = (unsigned int) q;
+	std::cout << q4 << std::endl;
+	std::cout << uqq << std::endl;
+	if (q4 != uqq)
+		err = 4;
+
+	unsigned long int q5 = (unsigned long int) q;
+	if (q5 != uqq)
+		err = 5;
+
+#ifndef __GIVARO__DONOTUSE_longlong__
+	unsigned long long int q6 = (unsigned long long int) q;
+	if (q6 != uqq)
+		err = 6 ;
+#endif
+
+
+	return (err != 0) ;
+
+}
 
 int main()
 {
-	int qq = -1 ;
-	Integer q(qq);
-	int q1           = (int) q;
-	long int q2      = (long int) q;
-	long long int q3 = (long long int) q;
-	if (q1 != qq)
-		return 1 ;
-	if (q2 != qq)
-		return 2 ;
-	if (q3 != qq)
-		return 3 ;
-	return 0 ;
+	int err = 0;
+	err  = testBasicConversion();
+	return err ;
+
 }

e29bc497e3c22ceb3624a43a15b3c1128569576d
jgdumas <jgdumas@dda9dfc7-2772-4c6c-972d-d99ad1e2f97f>
2011-11-30 10:28:12 +0000

    separate FF_EXPONENT_MAX from SUBEXPONENTMAX

--- trunk/src/kernel/zpz/givtablelimits.h
+++ trunk/src/kernel/zpz/givtablelimits.h
@@ -5,7 +5,7 @@
 // and abiding by the rules of distribution of free software.
 // see the COPYRIGHT file for more details.
 // file: givadicqfq.h
-// Time-stamp: <11 Jun 07 19:18:31 Jean-Guillaume.Dumas@imag.fr>
+// Time-stamp: <30 Nov 11 11:12:05 Jean-Guillaume.Dumas@imag.fr>
 // date: 2007
 // version:
 // author: Jean-Guillaume.Dumas
@@ -25,13 +25,13 @@
 #define __GIVARO_tablesize_MAX_H
 
 
-#ifndef FF_TABLE_MAX
+#ifndef _GIVARO_FF_TABLE_MAX
 // 2^23 ---> 2^23*4*3 = 100K
 // #define FF_TABLE_MAX 8388608UL
 // 2^20 ---> 2s on 735MHz
 //#define FF_TABLE_MAX 1048576UL
 // Now 2^21+1 seems OK
-#define FF_TABLE_MAX 2097153UL
+#define _GIVARO_FF_TABLE_MAX 2097153UL
 #endif
 
 #ifndef _GIVARO_FF_MAXEXPONENT_

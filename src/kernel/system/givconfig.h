abe9c57bd935443587d52f67ebbf8b7f2bf4fc93
bboyer <bboyer@dda9dfc7-2772-4c6c-972d-d99ad1e2f97f>
2013-06-12 15:53:05 +0000

    3.8 because of isOdd

--- src/kernel/system/givconfig.h
+++ src/kernel/system/givconfig.h
@@ -65,9 +65,9 @@
 // - yy: minor version number
 // - zz: revision number
 #define GIVARO_MAJOR_VERSION    3
-#define GIVARO_MINOR_VERSION    7
-#define GIVARO_REVISION_VERSION 2
-#define GIVARO_VERSION          30702
+#define GIVARO_MINOR_VERSION    8
+#define GIVARO_REVISION_VERSION 0
+#define GIVARO_VERSION          30800
 
 // -- Defines this value both to compile the library of user program
 // value: integer that defines debug level trace information (not well defined)

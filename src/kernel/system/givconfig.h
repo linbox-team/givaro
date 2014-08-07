f899af71df3a8443c6eb45f2fc1f9dd1b8be0069
jgdumas <jgdumas@dda9dfc7-2772-4c6c-972d-d99ad1e2f97f>
2014-08-07 13:10:56 +0000

    new version 3.8.1 --Tsss line, and those below, will be ignored--
    M    Makefile.am
    M    configure.ac
    M    src/Makefile.am
    M    src/kernel/system/givconfig.h

--- src/kernel/system/givconfig.h
+++ src/kernel/system/givconfig.h
@@ -66,8 +66,8 @@
 // - zz: revision number
 #define GIVARO_MAJOR_VERSION    3
 #define GIVARO_MINOR_VERSION    8
-#define GIVARO_REVISION_VERSION 0
-#define GIVARO_VERSION          30800
+#define GIVARO_REVISION_VERSION 1
+#define GIVARO_VERSION          30801
 
 // -- Defines this value both to compile the library of user program
 // value: integer that defines debug level trace information (not well defined)

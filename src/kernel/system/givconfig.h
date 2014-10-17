2a2015c0891b3c4c4b5547183b30ca42088fafea
bboyer <bboyer@dda9dfc7-2772-4c6c-972d-d99ad1e2f97f>
2014-10-17 18:11:23 +0000

    new version because it breaks fflas

--- src/kernel/system/givconfig.h
+++ src/kernel/system/givconfig.h
@@ -65,9 +65,9 @@
 // - yy: minor version number
 // - zz: revision number
 #define GIVARO_MAJOR_VERSION    3
-#define GIVARO_MINOR_VERSION    8
-#define GIVARO_REVISION_VERSION 2
-#define GIVARO_VERSION          30802
+#define GIVARO_MINOR_VERSION    9
+#define GIVARO_REVISION_VERSION 0
+#define GIVARO_VERSION          30900
 
 // -- Defines this value both to compile the library of user program
 // value: integer that defines debug level trace information (not well defined)

db8aaefe694f53ddb2e107706db7e7bbf77b8070
breusta <breusta@dda9dfc7-2772-4c6c-972d-d99ad1e2f97f>
2015-03-16 10:37:50 +0000

    Replaced override to final when possible

--- src/kernel/ring/modular-uint64.h
+++ src/kernel/ring/modular-uint64.h
@@ -79,8 +79,8 @@ public:
 	: zero(F.zero), one(F.one), mOne(F.mOne), _p(F._p) {}
 
 	// ----- Accessors
-	inline Element minElement() const override { return zero; }
-	inline Element maxElement() const override { return mOne; }
+	inline Element minElement() const final { return zero; }
+	inline Element maxElement() const final { return mOne; }
 
 	// ----- Access to the modulus
 	inline Residu_t residu() const { return _p; }
@@ -89,14 +89,14 @@ public:
 	inline Integer& characteristic(Integer& p) const { return p = _p; }
 	inline Residu_t cardinality() const { return _p; }
 	inline Integer& cardinality(Integer& p) const { return p = _p; }
-	static inline Residu_t getMaxModulus() { return 1073741824; }
+	static inline Residu_t getMaxModulus() { return 1073741824; } // 2^31
 	static inline Residu_t getMinModulus() { return 2; }
 
 	// ----- Checkers
-	inline bool isZero(const Element& a) const override { return a == zero; }
-	inline bool isOne (const Element& a) const override { return a == one; }
-	inline bool isMOne(const Element& a) const override { return a == mOne; }
-	inline bool areEqual(const Element& a, const Element& b) const override { return a == b; }
+	inline bool isZero(const Element& a) const final { return a == zero; }
+	inline bool isOne (const Element& a) const final { return a == one; }
+	inline bool isMOne(const Element& a) const final { return a == mOne; }
+	inline bool areEqual(const Element& a, const Element& b) const final { return a == b; }
 	inline size_t length(const Element a) const { return size_rep; }
 	
 	// ----- Ring-wise operators
@@ -129,34 +129,34 @@ public:
 	inline Element& reduce (Element& x) const { return x %= _p; }
 
 	// ----- Classic arithmetic
-	Element& mul(Element& r, const Element& a, const Element& b) const override;
-	Element& div(Element& r, const Element& a, const Element& b) const override;
-	Element& add(Element& r, const Element& a, const Element& b) const override;
-	Element& sub(Element& r, const Element& a, const Element& b) const override;
-	Element& neg(Element& r, const Element& a) const override;
-	Element& inv(Element& r, const Element& a) const override;
+	Element& mul(Element& r, const Element& a, const Element& b) const final;
+	Element& div(Element& r, const Element& a, const Element& b) const final;
+	Element& add(Element& r, const Element& a, const Element& b) const final;
+	Element& sub(Element& r, const Element& a, const Element& b) const final;
+	Element& neg(Element& r, const Element& a) const final;
+	Element& inv(Element& r, const Element& a) const final;
 
-	Element& mulin(Element& r, const Element& a) const override;
-	Element& divin(Element& r, const Element& a) const override;
-	Element& addin(Element& r, const Element& a) const override;
-	Element& subin(Element& r, const Element& a) const override;
-	Element& negin(Element& r) const override;
-	Element& invin(Element& r) const override;
+	Element& mulin(Element& r, const Element& a) const final;
+	Element& divin(Element& r, const Element& a) const final;
+	Element& addin(Element& r, const Element& a) const final;
+	Element& subin(Element& r, const Element& a) const final;
+	Element& negin(Element& r) const final;
+	Element& invin(Element& r) const final;
 	
 	// -- axpy:   r <- a * x + y
 	// -- axpyin: r <- a * x + r
-	Element& axpy  (Element& r, const Element& a, const Element& x, const Element& y) const override;
-	Element& axpyin(Element& r, const Element& a, const Element& x) const override;
+	Element& axpy  (Element& r, const Element& a, const Element& x, const Element& y) const final;
+	Element& axpyin(Element& r, const Element& a, const Element& x) const final;
 
 	// -- axmy:   r <- a * x - y
 	// -- axmyin: r <- a * x - r
-	Element& axmy  (Element& r, const Element& a, const Element& x, const Element& y) const override;
-	Element& axmyin(Element& r, const Element& a, const Element& x) const override;
+	Element& axmy  (Element& r, const Element& a, const Element& x, const Element& y) const final;
+	Element& axmyin(Element& r, const Element& a, const Element& x) const final;
 
 	// -- maxpy:   r <- y - a * x
 	// -- maxpyin: r <- r - a * x
-	Element& maxpy  (Element& r, const Element& a, const Element& x, const Element& y) const override;
-	Element& maxpyin(Element& r, const Element& a, const Element& x) const override;
+	Element& maxpy  (Element& r, const Element& a, const Element& x, const Element& y) const final;
+	Element& maxpyin(Element& r, const Element& a, const Element& x) const final;
 
 	// ----- Random generators
 	typedef ModularRandIter<Self_t> RandIter;

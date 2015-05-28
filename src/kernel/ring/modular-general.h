e16f514f9afbdcf265848da2c8eff0dd655bce96
breusta <breusta@dda9dfc7-2772-4c6c-972d-d99ad1e2f97f>
2015-05-28 07:52:01 +0000

    Added missing headers

--- src/kernel/ring/modular-general.h
+++ src/kernel/ring/modular-general.h
@@ -1,28 +1,37 @@
-// bb: c'est quoi ce fichier sans guardes, sans licence, sans auteur, sans modeline etc ?
+// ========================================================================
+// Copyright(c)'1994-2015 by The Givaro group
+// This file is part of Givaro.
+// Givaro is governed by the CeCILL-B license under French law
+// and abiding by the rules of distribution of free software.
+// see the COPYRIGHT file for more details.
+// Authors: A. Breust
+// Time-stamp: <28 May 15 09:40:00 Alexis.Breust@imag.fr>
+// ========================================================================
+// Description:
+// Forward declarations for Givaro::Modular and associated functions
 
 #pragma once
 
-// This file is necessary as default template parameters cannot
-// be redeclared. - A.B. 2015-18-03
-
 namespace Givaro
 {
-    // Elements will be stored in the storage type.
-    // While arithmetics will occur on the unsigned version of COMP type.
-    // Example: Modular<int32_t, uint64_t>
+    /*! Forward declaration for Givaro::Modular.
+     *  Elements will be stored in the storage type.
+     *  While arithmetics will occur on the unsigned version of COMP type.
+     *  Example: Modular<int32_t, uint64_t>
+     */
     template<typename Storage_t, typename COMP = Storage_t> class Modular;
 
-    //---- GCD
-
+    //! Generalized extended GCD used by specialized Modular.
     template<typename Storage_t>
     inline Storage_t& gcdext(Storage_t& d,  Storage_t& u, Storage_t& v, const Storage_t a, const Storage_t b);
 
+    //! Generalized inversion used by specialized Modular.
     template<typename Storage_t>
     inline Storage_t& invext(Storage_t& u, const Storage_t a, const Storage_t b);
 
+    //! Generalized inversion used by specialized Modular.
     template<typename Storage_t>
     inline Storage_t invext(const Storage_t a, const Storage_t b);
 }
 
 #include "givaro/modular-general.inl"
-

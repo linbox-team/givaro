752f3669cd6ac67881b2fe7f36ca0cf0232ca6aa
jgdumas <jgdumas@dda9dfc7-2772-4c6c-972d-d99ad1e2f97f>
2015-06-10 15:34:35 +0000

    cstor from int

--- trunk/src/kernel/ring/modular-inttype.h
+++ trunk/src/kernel/ring/modular-inttype.h
@@ -55,6 +55,16 @@ namespace Givaro
             assert(_p >= getMinModulus());
         }
 
+        template<class IntConvType>
+        Modular(const IntConvType& p, const IntConvType& e=1)
+            : zero(0)
+            , one(1)
+            , mOne( Caster<Residu_t>(p-1) )
+            , _p( Caster<Residu_t>(p) )
+        {
+            assert(_p >= getMinModulus());
+        }
+
         Modular(const Integer& p, const Integer& e=Integer::one)
             : zero(0)
             , one(1)

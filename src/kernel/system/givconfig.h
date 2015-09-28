cad2adc3c2daca93f8b899af3456c6e0f9e78457
Clement Pernet <clement.pernet@imag.fr>
2015-09-28 10:24:01 +0200

    increment version for next release

--- src/kernel/system/givconfig.h
+++ src/kernel/system/givconfig.h
@@ -66,8 +66,8 @@
 // - zz: revision number
 #define GIVARO_MAJOR_VERSION    4
 #define GIVARO_MINOR_VERSION    0
-#define GIVARO_REVISION_VERSION 0
-#define GIVARO_VERSION          40000
+#define GIVARO_REVISION_VERSION 1
+#define GIVARO_VERSION          40001
 
 // -- Defines this value both to compile the library of user program
 // value: integer that defines debug level trace information (not well defined)

1201bc079e71af2b3d9faa06d57c70b52ec28a13
Clément Pernet <clement.pernet@gmail.com>
2018-12-19 16:51:10 +0100

    preparing 4.1.0 release

--- src/kernel/system/givconfig.h
+++ src/kernel/system/givconfig.h
@@ -65,9 +65,9 @@
 // - yy: minor version number
 // - zz: revision number
 #define GIVARO_MAJOR_VERSION    4
-#define GIVARO_MINOR_VERSION    0
-#define GIVARO_REVISION_VERSION 5
-#define GIVARO_VERSION          40005
+#define GIVARO_MINOR_VERSION    1
+#define GIVARO_REVISION_VERSION 0
+#define GIVARO_VERSION          40100
 
 // -- Defines this value both to compile the library of user program
 // value: integer that defines debug level trace information (not well defined)

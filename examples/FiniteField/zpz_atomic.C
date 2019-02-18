3e552ac21bb00aecce5fc0249a98705448f474b5
Vincent Neiger <vincent.neiger@unilim.fr>
2019-02-18 16:22:29 +0100

    small fix

--- examples/FiniteField/zpz_atomic.C
+++ examples/FiniteField/zpz_atomic.C
@@ -168,8 +168,5 @@ int main(int argc, char ** argv)
 	return 0;
 }
 
-// vim:sts=8:sw=8:ts=8:noet:sr:cino=>s,f0,{0,g0,(0,\:0,t0,+0,=s:syntax=cpp.doxygen:foldmethod=syntax
-/* -*- mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
-
 /* -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 // vim:sts=4:sw=4:ts=4:et:sr:cino=>s,f0,{0,g0,(0,\:0,t0,+0,=s

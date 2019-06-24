1b9c69954fb0603d048e5d466a0911d1856c00a5
Jean-Guillaume Dumas <Jean-Guillaume.Dumas@imag.fr>
2019-06-24 12:15:12 +0200

    Solves issue #121

--- tests/test-ffarith.C
+++ tests/test-ffarith.C
@@ -153,6 +153,8 @@ int main(int argc, char ** argv)
     TEST_SPECIFIC(GFqDom<int64_t>, GFLLplarge, 4194301);
 
     TEST_LAST_PRIME(GFqDom<int32_t>, GFpmmax);
+        // int64_t maxCardinality would require 96GB 
+        // and about 960 CPU seconds ...
     TEST_SPECIFIC(GFqDom<int64_t>, GFLLpXXL, 67108859); // already 1.5 GB
 
     // Zech log finite field with 256 elements

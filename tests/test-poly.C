6174f2fcb45bb525439abbdc088795e5c869e67c
Jean-Guillaume Dumas <Jean-Guillaume.Dumas@imag.fr>
2022-01-04 17:44:19 +0100

    test polynomial isDivisor

--- tests/test-poly.C
+++ tests/test-poly.C
@@ -38,20 +38,26 @@ int main(int argc, char ** argv) {
 
     bool success = true;
 
+        // Testing exponentiation in Quotient Domain
     uint64_t n = generator() % expomax;
-
     QuotRing::Element R1, R2, P;
-
     PD.random(generator,P,Degree(degmax));
     dom_power(R1, P, n, QD);
-
     PD.powmod(R2, P, Integer(n), Q);
-
     success &= PD.areEqual(R1,R2);
-
     if (! success) {
         std::cerr << "Error: " << seed << std::endl;
         PD.write(PD.write(std::cerr << "R1: ", R1) << " != ", R2) << std::endl;
+    }
+
+
+        // Testing divisibility in Polynomial Domain
+    PD.mul(R1, P, Q);
+    success &= PD.isDivisor(R1, P) && PD.isDivisor(R1, Q);
+    if (! success) {
+        std::cerr << "Error: " << seed << std::endl;
+        PD.write(PD.write(PD.write(
+            std::cerr, P) << "\n or ", Q) << "\n does not divide ", R1) << std::endl;
     }
 
     return (! success);
